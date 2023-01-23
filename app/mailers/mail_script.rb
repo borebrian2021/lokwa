b0c997270975c35515c013ac4a223c99564ca39d
borebrian2021 <bkimutai2021@gmail.com>
2023-01-23 12:15:26 +0300

    Added some mail tricks

--- app/mailers/mail_script.rb
+++ app/mailers/mail_script.rb
@@ -1,18 +1,20 @@
 require 'net/smtp'
-FROM_EMAIL = "bkimutai2021@gmail.com"
-PASSWORD = "cwufeiruewdwcvhh"
-TO_EMAIL = "bkimutai2021@gmail.com"
-smtp = Net::SMTP.new 'smtp.gmail.com', 587
-message = <<END_OF_MESSAGE
+module MailScript
+def send_message(message_,to)
+@FROM_EMAIL = "bkimutai2021@gmail.com"
+@PASSWORD = "cwufeiruewdwcvhh"
+@TO_EMAIL = to
+@smtp = Net::SMTP.new 'smtp.gmail.com', 587
+@message = <<END_OF_MESSAGE
 From: Lokwa Innovation <bkimutai2021@gmail.com>
 To: ToName <bkimutai2021@gmail.com>
-Subject: Mail From Ruby 
+Subject: Account activation code.
 Content-Type: text/html
 Content-Transfer-Encoding:8bit
-<strong>The beginning of your HTML content.</strong>
-<h1>And some headline, as well.</h1>
+<strong>#{message_}</strong>
+<p>Lokwa Team.</p>   
 END_OF_MESSAGE
-
-smtp.start('received-from-goes-here', FROM_EMAIL, PASSWORD, :plain)
-smtp.send_message(message, FROM_EMAIL, TO_EMAIL)
-smtp.finish()
+@smtp.start('received-from-goes-here', @FROM_EMAIL, @PASSWORD, :plain)
+@smtp.finish() 
+end
+end
